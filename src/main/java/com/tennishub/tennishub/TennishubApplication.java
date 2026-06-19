bd33e91abc5e428231bc05f4958860106a00e294
ahetru <axel.a.hetru@gmail.com>
2026-06-19 15:48:24 +0200

    add basic spring structure to project

---
+++ src/main/java/com/tennishub/tennishub/TennishubApplication.java
@@ -0,0 +1,13 @@
+package com.tennishub.tennishub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennishubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennishubApplication.class, args);
+    }
+
+}
